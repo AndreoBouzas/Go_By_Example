ed7aec0959afbdb4e4526cc6e306069662bff5fc
Andreo <andreobouzas@gmail.com>
2021-12-22 13:20:45 -0300

    Adição do package Hello World

---
+++ Hello_World/Hello_World.go
@@ -0,0 +1,14 @@
+package HelloWorld //Declarando o nome do package
+
+import ( //Importando os packages
+	"fmt"
+
+	Explain "github.com/AndreoBouzas/Go_By_Example/Explain"
+)
+
+//Declarando a função principal
+func HelloWorld() {
+	Explain.Explain
+	//Print na tela da mensagem definida!
+	fmt.Println("Hello World")
+}

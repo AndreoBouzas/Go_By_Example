8c1f9df71976c44ba20c6a008ce8de8a9adcd777
Andreo <andreobouzas@gmail.com>
2021-12-22 17:14:47 -0300

    Correção da estrutura do código

--- Values/Values.go
+++ Values/Values.go
@@ -7,33 +7,21 @@ import (
 )
 
 func Values() {
+	fmt.Print("Exemplo 2  (Valores e seus tipos): \n\n")
 	fmt.Println(Explain.ExplainThis(2))
-	fmt.Print(` 
-	
-	func main() {
-
-		Strings, Podem ser adicionadas com o +.
-			fmt.Println("go" + "lang")
-
-
-		Integers e floats.
-			fmt.Println("1+1 =", 1+1)
-			fmt.Println("7.0/3.0 =", 7.0/3.0)
-
-
-		Booleans, uso dos operadores booleanos como esperado, true/ false
-			fmt.Println(true && false)
-			fmt.Println(true || false)
-			fmt.Println(!true)
-		}  
-		` + "\n\n")
-
-	fmt.Print("Os resultados são: \n\n")
-	fmt.Println("go" + "lang")
+	fmt.Print("Strings, Podem ser adicionadas com o + \n EXEMPLO : \n" + `
+	fmt.Println("go" + "lang")` + "\n\n RESULTADO : \n\n")
+	fmt.Print("go" + "lang\n\n")
+	fmt.Print("Integers e floats.\n EXEMPLO : \n" + `
 	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)` + "\n\n RESULTADO : \n\n")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Print("7.0/3.0 =", 7.0/3.0, "\n\n")
+	fmt.Print("Booleans, uso dos operadores booleanos como esperado, true/ false \n EXEMPLO : \n" + `
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)` + "\n\n RESULTADO : \n\n")
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
-
 }

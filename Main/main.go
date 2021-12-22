c90a59d1c60598611a2ea4ae578d6f1e67e9590e
Andreo <andreobouzas@gmail.com>
2021-12-22 17:15:31 -0300

    Adição da chamada do example If_Else

--- Main/main.go
+++ Main/main.go
@@ -3,12 +3,23 @@ package main
 import (
 	"fmt"
 
+	Constants "github.com/AndreoBouzas/Go_By_Example/Constants"
+	For "github.com/AndreoBouzas/Go_By_Example/For"
 	Hello_World "github.com/AndreoBouzas/Go_By_Example/Hello_World"
+	If_Else "github.com/AndreoBouzas/Go_By_Example/If_Else"
 	Values "github.com/AndreoBouzas/Go_By_Example/Values"
+	Variables "github.com/AndreoBouzas/Go_By_Example/Variables"
 )
 
 func main() {
-	fmt.Println("Digite o número do exemplo que gostaria de ohar! :")
+	fmt.Println("Digite o NÚMERO do exemplo que gostaria de ohar! :")
+	fmt.Println("Exemplo 1  (Hello World!)")
+	fmt.Println("Exemplo 2  (Valores e seus tipos)")
+	fmt.Println("Exemplo 3  (Variáveis e Declarações)")
+	fmt.Println("Exemplo 4  (Constantes)")
+	fmt.Println("Exemplo 5  (Laço For)")
+	fmt.Println("Exemplo 6  (If/Else)")
+	fmt.Println("")
 	exampleNumber := 0
 	fmt.Scan(&exampleNumber)
 
@@ -16,6 +27,14 @@ func main() {
 		Hello_World.HelloWorld()
 	} else if exampleNumber == 2 {
 		Values.Values()
+	} else if exampleNumber == 3 {
+		Variables.Variables()
+	} else if exampleNumber == 4 {
+		Constants.Constants()
+	} else if exampleNumber == 5 {
+		For.For()
+	} else if exampleNumber == 6 {
+		If_Else.If_Else()
 	}
 
 }

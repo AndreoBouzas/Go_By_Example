22e502f31840d579151d2bedfec8d7fa6065b101
Andreo <andreobouzas@gmail.com>
2021-12-22 17:13:03 -0300

    Adição da explicação do exemplo If e Else

--- Explain/Explain.go
+++ Explain/Explain.go
@@ -11,6 +11,14 @@ func ExplainThis(exampleNumber int) string {
 		return finaltext
 	} else if exampleNumber == 2 {
 		finaltext = fmt.Sprintln("Este programa aprenta os tipos primitivos de valores em go!")
+	} else if exampleNumber == 3 {
+		finaltext = fmt.Sprintln("Este programa aprenta as formas de declarar e inicializar as variáveis!")
+	} else if exampleNumber == 4 {
+		finaltext = fmt.Sprintln("Este programa aprenta as constantes, formas de declarar e inicializa-las!")
+	} else if exampleNumber == 5 {
+		finaltext = fmt.Sprintln("for é a única construção de loop de Go. Aqui estão alguns tipos básicos de forloops!")
+	} else if exampleNumber == 6 {
+		finaltext = fmt.Sprintln("If e Else, são condições de verificação que determinam diferentes comportamentos do código")
 	}
 	return finaltext
 }
